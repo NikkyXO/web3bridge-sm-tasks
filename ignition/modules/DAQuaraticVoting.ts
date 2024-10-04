aeae0e3c4111506d180e6e7f30b3adbdf41a1a45
Olanike <alexisdame2017@gmail.com>
2024-10-04 16:59:10 +0100

    add dependencies

--- ignition/modules/DAQuaraticVoting.ts
+++ ignition/modules/DAQuaraticVoting.ts
@@ -1,10 +1,10 @@
 import { buildModule } from "@nomicfoundation/hardhat-ignition/modules";
 
-
+const tokenAddress1 = "0x5cD571919dF92B190850157BB2bC2f2e1a1e4245";
 const DAQuadraticVotingModule = buildModule("DAQuadraticVotingModule", (m) => {
 
 
-  const daouadraticVoting = m.contract("DAOQuadraticVoting");
+  const daouadraticVoting = m.contract("DAOQuadraticVoting", [tokenAddress1]);
 
   return { daouadraticVoting  };
 });
